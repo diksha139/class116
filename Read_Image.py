8dd88e73c006a6dba710686b7af589d0620b1c62
diksha139 <108606854+diksha139@users.noreply.github.com>
2022-10-28 18:56:08 +0530

    Add files via upload

---
+++ Read_Image.py
@@ -0,0 +1,17 @@
+import cv2
+
+# Read Image
+img = cv2.imread("butterfly.jpg")
+
+# Display Colored Image
+# cv2.imshow("Colored Image",img)
+
+# Convert Colored Image To Grayscale
+gray_img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+
+# Display Grayscale Image
+cv2.imshow("Grayscale", gray_img)
+
+
+#print(gray_img)
+cv2.waitKey(0)
